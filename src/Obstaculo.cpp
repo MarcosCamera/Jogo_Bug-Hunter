520f23b46a852dc8fd0e8d09c99b85d2216d9a98
isabellaeurich <eurichisabella@gmail.com>
2025-11-07 16:47:05 -0300

    Implement Obstaculo class in Obstaculo.cpp

---
+++ src/Obstaculo.cpp
@@ -0,0 +1,12 @@
+#include "Obstaculo.hpp"
+using namespace std;
+
+
+using namespace Entidades::Obstaculos;
+
+Obstaculo::Obstaculo() : Entidade(), danoso(){}
+
+Obstaculo::Obstaculo(const std::string& caminhoSprite,sf::Vector2f posicao):
+Entidade(caminhoSprite, posicao), danoso(){}
+
+Obstaculo::~Obstaculo(){}
