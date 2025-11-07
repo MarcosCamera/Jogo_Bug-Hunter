45da1d81778c19907f4f51db61bedaa11f150872
isabellaeurich <eurichisabella@gmail.com>
2025-11-07 16:52:23 -0300

    Add Plataforma class definition in Plataforma.hpp

---
+++ src/Plataforma.hpp
@@ -0,0 +1,20 @@
+#include "Obstaculo.hpp"
+#include "Jogador.hpp"
+#pragma once
+
+namespace Entidades::Obstaculos
+{
+    class Plataforma:public Obstaculo
+    {
+        protected:
+           int altura;
+            
+         public:
+          Plataforma();
+          Plataforma(const std::string& caminhoSprite, sf::Vector2f position);
+          ~Plataforma();
+
+          void executar();
+          void obstaculizar(Personagens::Jogador* p);
+    };
+}
