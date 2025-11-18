102ecb3cffa3b6bebacae2bf1051964ddc2f3074
isabellaeurich <eurichisabella@gmail.com>
2025-11-18 20:07:58 -0300

    Update Fase_Primeira.hpp

--- src/Fase_Primeira.hpp
+++ src/Fase_Primeira.hpp
@@ -8,15 +8,15 @@ namespace Fases
     class Fase_Primeira : public Fase
     {
         private:
-          const int max;
-          const int min;
-          std::vector<sf::Vector2f> formigaSpawnPoints;
-          std::vector<sf::Vector2f> griloSpawnPoints;
-          std::vector<sf::Vector2f> formigueiroSpawnPoints;
+          const int maxFormigueiro;
+          const int minFormigueiro;
+          const int maxGrilo;
+          const int minGrilo;
+          
         
         protected:
-          void criarInimMedios(float posX, float posY);
-          void criarObstMedios(float posX, float posY, int id_tile);
+          void criarInimMedios();
+          void criarObstMedios();
           
         public:
            Fase_Primeira(Gerenciadores::Gerenciador_Grafico* pGG, Gerenciadores::Gerenciador_Colisoes& gC, std::string caminho);
