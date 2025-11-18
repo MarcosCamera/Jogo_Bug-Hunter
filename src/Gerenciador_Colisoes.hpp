fba29a73384b0e42fb5b3ac861450d29c0fa6a3a
isabellaeurich <eurichisabella@gmail.com>
2025-11-17 22:39:31 -0300

    Update Gerenciador_Colisoes.hpp

--- src/Gerenciador_Colisoes.hpp
+++ src/Gerenciador_Colisoes.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <vector>
 #include <list>
-#include <set>
 #include "Inimigo.hpp"
 #include "Jogador.hpp"
 #include "Obstaculo.hpp"
@@ -16,9 +15,8 @@ namespace Gerenciadores
     class Gerenciador_Colisoes
     {
         private:
-         vector<Inimigo*> LIs;
+         vector<Inimigo*>  LIs;
          list<Obstaculo*> LOs;
-         set<Projetil*> LPs;
          Jogador* pJog1;
         
          public:
@@ -27,19 +25,17 @@ namespace Gerenciadores
 
            void incluirInimigo(Inimigo* pi);
            void incluirObstaculo(Obstaculo* po);
-           void incluirProjetil(Projetil* pj);
            void setJogador(Jogador* pJ);
-           //void incluirEntidade()   //fazer para incluir qualquer tipo de entidade e chamar as funçoes "tratar colisoes" (ou melhor nao)
            void executar();
 
         private:
-         const bool verificarColisao(Entidade* pe1, Entidade* pe2) const;
+         const bool verificarColisao(Entidade* p1, Entidade* p2) const;
          
          void tratarColisoesJogsObstacs();
-         void tratarColisoesJogsInimigs(); 
-         void tratarColisoesJogsProjeteis();
-         void tratarColisoesInimigsObstacs(); //nao estava no UML padrao
-         void tratarColisoesInimigsObstacs(); //nao estava no UML padrao
+         void tratarColisoesJogsInimigs();
+         void tratarColisoesInimigsObstacs();
+
+
 
     };
 }
