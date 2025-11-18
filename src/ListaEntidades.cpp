fd1abf10a9c2ff314627ff6c2da12f02f80c95ac
isabellaeurich <eurichisabella@gmail.com>
2025-11-17 22:18:58 -0300

    Add files via upload

---
+++ src/ListaEntidades.cpp
@@ -0,0 +1,54 @@
+#include "ListaEntidades.hpp"
+#include "Entidade.hpp"
+#include "Lista.hpp"
+#include "Gerenciador_Grafico.hpp"
+#include <iostream>
+using namespace  std;
+
+using Entidades::Entidade;
+using namespace Listas;
+using Iterador = Listas::Lista<Entidades::Entidade>::Iterador<Entidades::Entidade>;
+
+    ListaEntidades::ListaEntidades() {}
+    ListaEntidades::~ListaEntidades() 
+    {
+        LEs.limpar();
+    }
+    
+    Lista<Entidades::Entidade>* ListaEntidades::getLista()
+    {
+        return &LEs;
+    }
+
+    void ListaEntidades::incluir(Entidade* pE)
+    {
+        if (pE)
+        {
+            LEs.incluir(pE);
+        }
+        else
+         cout<<"ListaEntidades::incluir() -> Entidade* pE NULL"<<endl;
+    }
+
+    void ListaEntidades::percorrer()
+    {
+        
+        Iterador it = LEs.getIterador();
+
+        while (!it.end())
+        {
+            
+            Entidades::Entidade* pEntidade = it.getElemento();
+            if (pEntidade)
+            {
+                pEntidade->executar();
+            }
+            it.proximo();
+        }
+    }
+
+
+    void ListaEntidades::limpar()
+    {
+        LEs.limpar();
+    }
---
+++ src/ListaEntidades.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "Lista.hpp"
+
+
+namespace Entidades {
+    class Entidade;
+}
+using Iterador = Listas::Lista<Entidades::Entidade>::Iterador<Entidades::Entidade>;
+
+namespace Listas
+{
+    class ListaEntidades
+    {
+    private:
+        Lista<Entidades::Entidade> LEs;
+
+    public:
+        ListaEntidades();
+        ~ListaEntidades();
+        Lista<Entidades::Entidade>* getLista();
+        void incluir(Entidades::Entidade* pE);//pq dois incluir???
+        void percorrer();
+        void limpar();
+    };
+
+}
+
