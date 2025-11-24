7098546f7aa942ba540c90e1f709fa597575b62d
isabellaeurich <eurichisabella@gmail.com>
2025-11-24 06:47:22 -0300

    Update Entidade.hpp

--- src/Entidade.hpp
+++ src/Entidade.hpp
@@ -1,36 +1,48 @@
 #pragma once
-
 #include <SFML/Graphics.hpp>
 #include "Ente.hpp"
+
+
 
 namespace Entidades{
 class Entidade : public Ente{
    protected:
-      bool chao;
-      sf::Vector2f vel;
-       sf::Vector2f acel;
-       bool direcao;
-      const float gravidade;
-      const float velMovMax;
-      const float arrasto;
-      float normal;
+  sf::Vector2f pos;
+  sf::Vector2f vel;
+  sf::Vector2f acel;
+  const float gravidade;
+  float normal;
+  const float arrasto;
+  float velMovMax; 
+  bool direcao; 
+  bool chao; 
       
 
     public:
 
-      Entidade(sf::Vector2f posicao);
+      Entidade(sf::Vector2f posicao);//sobrecarga construtora
       virtual ~Entidade();
 
       virtual void executar() = 0;
-      void setVel(sf::Vector2f velocidade);
-      
+      virtual void mover() = 0;
 
+      void setVel(sf::Vector2f velocidade);
+      void setPos(sf::Vector2f novapos);
       sf::Vector2f getVel() const;
       sf::Vector2f getPos() const;
+
       void acelerar(); 
       void atualizaVel();
+      void atualizaPos();
 
        void setChao(bool c);
+       bool getChao() const;
+
+      void mudaDir(); 
+      bool getDir() const;
+      void setDir(bool d);
+
+    
       
   };
 }
