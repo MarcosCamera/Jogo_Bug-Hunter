48d211ff6f3cbc9195764a5f022796da40fbd7a8
isabellaeurich <eurichisabella@gmail.com>
2025-11-24 07:04:54 -0300

    Update Jogo.hpp

--- src/Jogo.hpp
+++ src/Jogo.hpp
@@ -1,12 +1,21 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "Jogador.hpp"
-#include "Inim_Facil.hpp"
+#include "Grilo.hpp"
+#include "Formiga.hpp"
 #include "ListaEntidades.hpp"
 #include "Fase.hpp"
 #include "Gerenciador_Grafico.hpp"
 #include "Formigueiro.hpp"
-#include "Plataforma.hpp"
+#include "Folha.hpp"
+#include "Gerenciador_Eventos.hpp"
+
+#include "Menu.hpp"
+#include "MenuPausa.hpp" 
+#include "MenuInicial.hpp"
+
+#include "Fase_Primeira.hpp"
+#include "Fase_Segunda.hpp"
 
 using namespace Gerenciadores;
 using namespace Entidades::Personagens;
@@ -14,10 +23,18 @@ using namespace Entidades::Obstaculos;
 
 class Jogo{
    private:
-    Fases::Fase* pF1;
-    Gerenciador_Grafico* pGrafico;
-    Gerenciador_Colisoes pColisoes;
     Fases::Fase* pFaseAtual;
+    Fases::Fase_Primeira* pF1;
+    Fases::Fase_Segunda* pF2;
+    
+    Gerenciador_Grafico* pGrafico;
+    Gerenciador_Eventos* pEventos;
+    Gerenciador_Colisoes pColisoes;
+    
+
+    MenuInicial menuInicial;
+    MenuPausa menuPausa;
+    int estadoDoJogo; //(0==menu, 1==jogo, 2==sair)
     
     
 
