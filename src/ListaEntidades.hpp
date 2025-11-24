1353029d2cb947980a6b80c57523d04f9037b853
isabellaeurich <eurichisabella@gmail.com>
2025-11-23 22:29:46 -0300

    remover implementada

--- src/ListaEntidades.hpp
+++ src/ListaEntidades.hpp
@@ -21,6 +21,7 @@ namespace Listas
         void incluir(Entidades::Entidade* pE);//pq dois incluir???
         void percorrer();
         void limpar();
+        void retirar(Entidades::Entidade* pE = NULL);
     };
 
 }
