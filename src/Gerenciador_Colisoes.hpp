6329f52bbd3030be63087fce32a6d5508343b522
isabellaeurich <eurichisabella@gmail.com>
2025-11-24 06:54:44 -0300

    Update Gerenciador_Colisoes.hpp

--- src/Gerenciador_Colisoes.hpp
+++ src/Gerenciador_Colisoes.hpp
@@ -1,47 +1,53 @@
 #pragma once
+
 #include <vector>
 #include <list>
+#include <set>
 #include "Inimigo.hpp"
 #include "Jogador.hpp"
 #include "Obstaculo.hpp"
+#include "Projetil.hpp"
 #include "Parede.hpp"
 
 using namespace std;
-using namespace Entidades::Personagens;
-using namespace Entidades::Obstaculos;
-using namespace Entidades;
 
 namespace Gerenciadores
 {
     class Gerenciador_Colisoes
     {
         private:
-         vector<Inimigo*>  LIs;
-         list<Obstaculo*> LOs;
-         Jogador* pJog1;
-          Parede* pParedeChao;
-         
-
+            vector<Entidades::Personagens::Inimigo*> LIs;
+            list<Entidades::Obstaculos::Obstaculo*> LOs;
+            list<Entidades::Parede*>LPas;
+            set<Entidades::Projetil*> LPs;
+            Entidades::Personagens::Jogador* pJog1;
+            Entidades::Personagens::Jogador* pJog2;
         
-         public:
-           Gerenciador_Colisoes();
-           ~Gerenciador_Colisoes();
-
-           void incluirInimigo(Inimigo* pi);
-           void incluirObstaculo(Obstaculo* po);
-           void setJogador(Jogador* pJ);
-           void setParede(Parede* pP);
-           void executar();
+        public:
+            Gerenciador_Colisoes();
+            ~Gerenciador_Colisoes();
 
         private:
-         const bool verificarColisao(Entidade* p1, Entidade* p2) const;
-         
-         void tratarColisoesJogsObstacs();
-         void tratarColisoesJogsInimigs();
-         void tratarColisoesInimigsObstacs();
-         void tratarColisoesParedeEntidades();
+            const bool verificarColisao(Entidades::Entidade* pe1, Entidades::Entidade* pe2) const;
+            void tratarColisoesJogsObstacs();
+            void tratarColisoesJogsInimigs(); 
+            void tratarColisoesJogsProjeteis();
+            void tratarColisoesInimigsObstacs();
+            void tratarColisoesInimigsProjeteis();
+            void tratarColisoesObstacsProjeteis();
+            void tratarColisoesJogsParedes();
+            void tratarColisoesInimigsParedes();
+            void tratarColisoesProjeteisParedes();
+            void tratarColisoesObstacsParedes();
+            //InimigsInimigs
 
-
-
+        public:
+            void incluirInimigo(Entidades::Personagens::Inimigo* pi);
+            void incluirObstaculo(Entidades::Obstaculos::Obstaculo* po);
+            void incluirParede(Entidades::Parede* pp);
+            void incluirProjetil(Entidades::Projetil* pj);
+            void setJogador(Entidades::Personagens::Jogador* pJ);
+            void setJogador(Entidades::Personagens::Jogador* pJ1, Entidades::Personagens::Jogador* pJ2);
+            void executar();
     };
 }
