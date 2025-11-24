63766dd27cf09368c9994acd20442863c3608b7f
isabellaeurich <eurichisabella@gmail.com>
2025-11-24 07:03:39 -0300

    Update Inimigo.hpp

--- src/Inimigo.hpp
+++ src/Inimigo.hpp
@@ -4,16 +4,33 @@
 
 
 
-namespace Entidades::Personagens{
-class Inimigo : public Personagem{
-  protected:
-     int nivel_maldade;
-  public:
-    Inimigo();
-    virtual ~Inimigo();
-    virtual void executar() = 0;
-    virtual void mover() = 0;
-    virtual void danificar(Jogador* p) = 0;
-     
-};
+namespace Entidades
+{
+    namespace Personagens
+    {
+        class Formiga;
+        class Grilo;
+        class LouvaDeus;
+        class Inimigo : public Personagem
+        {
+        protected:
+            int nivel_maldade;
+            
+
+        public:
+            Inimigo();
+            Inimigo(sf::Vector2f pos); 
+            virtual ~Inimigo();
+            void operator++();  //nivel_maldade++
+            void operator--();  //nivel_maldade--
+
+            void setMaldade(int m);
+            int getMaldade();
+            
+            virtual void danificar(Jogador* p) = 0;
+            virtual void mover() = 0;
+            virtual void executar() = 0;
+            
+        };
+    }
 }
