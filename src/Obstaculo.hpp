22b9a7a058606e5a4b8ef3fa3c005ddd76c5e37c
isabellaeurich <eurichisabella@gmail.com>
2025-11-24 07:14:02 -0300

    Update Obstaculo.hpp

--- src/Obstaculo.hpp
+++ src/Obstaculo.hpp
@@ -4,19 +4,27 @@
 #include "Entidade.hpp"
 #include "Jogador.hpp"
 
-namespace Entidades::Obstaculos
+namespace Entidades
 {
-  class Obstaculo : public Entidade{
-     protected:
-        bool danoso;
-     public:
-       Obstaculo();
-       Obstaculo(const std::string& caminhoSprite, sf::Vector2f posicao);
-       virtual ~Obstaculo();
-       
-       virtual void obstaculizar(Personagens::Jogador* p) = 0;
-       virtual void executar()= 0;
-       virtual void setDano(bool dano)=0;
-       virtual bool getDano() = 0;
+    namespace Obstaculos
+    {
+        class Folha;
+        class PlantaCarnivora;
+        class Formigueiro;
 
-  };
+        class Obstaculo : public Entidade 
+        {
+        protected:
+            bool danoso;
+            int timer;
+
+        public:
+            Obstaculo();
+            virtual ~Obstaculo();
+
+            virtual void obstaculizar(Personagens::Personagem* pE) = 0;
+            virtual void mover();
+            virtual void executar() = 0;
+        };
+    }
+}
