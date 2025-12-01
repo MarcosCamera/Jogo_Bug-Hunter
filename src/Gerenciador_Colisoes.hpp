9b3a347954094d5870a6c2bc1c7b51b541ffda85
isabellaeurich <eurichisabella@gmail.com>
2025-11-30 21:45:57 -0300

    mudei

--- src/Gerenciador_Colisoes.hpp
+++ src/Gerenciador_Colisoes.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <vector>
 #include <list>
 #include <set>
@@ -8,24 +7,36 @@
 #include "Obstaculo.hpp"
 #include "Projetil.hpp"
 #include "Parede.hpp"
+#include "Teia.hpp"
+
 
 using namespace std;
+using namespace Entidades; 
+using namespace Obstaculos;
+using namespace Personagens;
 
 namespace Gerenciadores
 {
     class Gerenciador_Colisoes
     {
         private:
+            static Gerenciador_Colisoes* pInstancia;
+
+            Gerenciador_Colisoes();
+
             vector<Entidades::Personagens::Inimigo*> LIs;
             list<Entidades::Obstaculos::Obstaculo*> LOs;
             list<Entidades::Parede*>LPas;
             set<Entidades::Projetil*> LPs;
+            std::list<Teia*> LTeias;                    
             Entidades::Personagens::Jogador* pJog1;
             Entidades::Personagens::Jogador* pJog2;
+
         
         public:
-            Gerenciador_Colisoes();
+            
             ~Gerenciador_Colisoes();
+            static Gerenciador_Colisoes* getInstancia();
 
         private:
             const bool verificarColisao(Entidades::Entidade* pe1, Entidades::Entidade* pe2) const;
@@ -39,6 +50,10 @@ namespace Gerenciadores
             void tratarColisoesInimigsParedes();
             void tratarColisoesProjeteisParedes();
             void tratarColisoesObstacsParedes();
+
+            void tratarColisoesTeiasInimigos();         
+            void tratarColisoesTeiasParedes(); 
+
             //InimigsInimigs
 
         public:
@@ -46,6 +61,11 @@ namespace Gerenciadores
             void incluirObstaculo(Entidades::Obstaculos::Obstaculo* po);
             void incluirParede(Entidades::Parede* pp);
             void incluirProjetil(Entidades::Projetil* pj);
+            void incluirTeia(Teia* pt);
+
+            void removerInimigo(Entidades::Personagens::Inimigo* pi);
+            void removerTeia(Teia* pt);
+
             void setJogador(Entidades::Personagens::Jogador* pJ);
             void setJogador(Entidades::Personagens::Jogador* pJ1, Entidades::Personagens::Jogador* pJ2);
             void executar();
