ea2975636bcee5e7724d693e0d2f0f05ad3b5834
isabellaeurich <eurichisabella@gmail.com>
2025-11-30 21:51:09 -0300

    mudei

--- src/Jogador.cpp
+++ src/Jogador.cpp
@@ -7,18 +7,49 @@ namespace Entidades
 {
 	namespace Personagens
 	{
-		Jogador::Jogador(sf::Vector2f posicao) : Personagem(), pontos(0)
+        Jogador::Jogador():Personagem(sf::Vector2f(0.0f, 0.0f), NULL), pontos(0.0f),cooldownTiro(0.5f),tempoCooldown(0.0f), tiro(false)
+		{
+		try 
+		
+            {
+                
+                setTexture("Textures/spider2.png", sf::Vector2f(2.0f, 2.0f));
+                if (pFig) {
+					sf::FloatRect bounds = pFig->getLocalBounds();
+					pFig->setOrigin(bounds.width / 2.f, bounds.height);
+
+					sf::Vector2f posAtual = getPos();
+
+					float tileLargura = 32.0f; 
+					float tileAltura = 32.0f;
+					sf::Vector2f centroBaseTile(posAtual.x + tileLargura / 2.f, posAtual.y + tileAltura);
+					pFig->setPosition(centroBaseTile);
+				
+               }
+               else
+                 cout<<"Jogador -> pFig NULL"<< endl;
+            }
+            catch (const std::exception& e) 
+            {
+                std::cerr << e.what() << std::endl;
+            }
+		}
+
+		Jogador::Jogador(sf::Vector2f pos, Listas::ListaEntidades* p ) : Personagem(pos, p), pontos(0), cooldownTiro(0.5f), tempoCooldown(0.0f)
         {
-        
-            impacto = 1;
-            num_vidas = 10;
 			try 
             {
                 
                 setTexture("Textures/spider2.png", sf::Vector2f(2.0f, 2.0f));
                 if (pFig) {
-                   sf::FloatRect bounds = pFig->getGlobalBounds();
-                   pFig->setOrigin(bounds.width / 2.f, bounds.height / 2.f);
+					sf::FloatRect bounds = pFig->getLocalBounds();
+					pFig->setOrigin(bounds.width / 2.f, bounds.height);
+
+					float tileLargura = 32.0f; 
+					float tileAltura = 32.0f;
+
+					sf::Vector2f centroBaseTile(pos.x + tileLargura / 2.f, pos.y + tileAltura);
+					pFig->setPosition(centroBaseTile);
                }
                else
                  cout<<"Jogador -> pFig NULL"<< endl;
@@ -28,13 +59,17 @@ namespace Entidades
                 std::cerr << e.what() << std::endl;
             }
             
-            setVidas(10);
+            
         }
 
 
 		Jogador::~Jogador()
 		{
-			
+			if (pFig)
+			{
+				delete pFig;
+				pFig = NULL;
+			}
 		}
 
 		void Jogador::danificarInim(Inimigo* pIn)
@@ -43,121 +78,146 @@ namespace Entidades
 		}
 
 		void Jogador::colidir(Inimigo* pIn)
-{
-    sf::FloatRect intersec;
-    sf::FloatRect inimigoBounds = pIn->getFig()->getGlobalBounds();
-    sf::FloatRect jogadorBounds = this->getFig()->getGlobalBounds();
-
-    if (inimigoBounds.intersects(jogadorBounds, intersec))
-    {
-        sf::Vector2f novaPos = pIn->getPos();
-        if (intersec.width < intersec.height)
         {
-            pIn->mudaDir();
+			sf::FloatRect intersec;
+			sf::FloatRect inimigoBounds = pIn->getFig()->getGlobalBounds();
+			sf::FloatRect jogadorBounds = this->getFig()->getGlobalBounds();
 
-            if (inimigoBounds.left < jogadorBounds.left)
-            {
-                pIn->danificar(this);
-                novaPos.x -= intersec.width;
-                vel = (sf::Vector2f(static_cast<float>(-pIn->getImpacto()), static_cast<float>(-pIn->getImpacto() / 2)));
-            }
-            else
-            {
-                pIn->danificar(this);
-                novaPos.x += intersec.width;
-                vel = (sf::Vector2f(static_cast<float>(pIn->getImpacto()), static_cast<float>(-pIn->getImpacto() / 2)));
-            }
-        }
-        else
-        {
-            if (inimigoBounds.top < jogadorBounds.top)
-            {
-                pIn->danificar(this);
-                novaPos.y -= intersec.height;
-                pIn->setVel(sf::Vector2f(pIn->getVel().x, -pIn->getVel().y));
-            }
-            else
-            {
-                danificarInim(pIn);
-                
-                sf::Vector2f currentPos = getPos();
-                currentPos.y -= intersec.height;
-                pFig->setPosition(currentPos);
-                
-                vel.y = -vel.y;
-            }
-        }
+			if (inimigoBounds.intersects(jogadorBounds, intersec))
+			{
+				sf::Vector2f novaPos = pIn->getPos();
+				if (intersec.width < intersec.height) //colisao horizontal
+				{
+					pIn->mudaDir(); //talvez nao seja necessario o if seguinte?
 
-        pIn->setPos(novaPos);
-    }
-}
+					if (inimigoBounds.left < jogadorBounds.left) //inimigo esquerda
+					{
+						pIn->danificar(this);
+						novaPos.x -= intersec.width;
+						vel = (sf::Vector2f(static_cast<float>(-pIn->getImpacto()), static_cast<float>(-pIn->getImpacto() / 2)));
+					}
+					else //inimigo direita
+					{
+						pIn->danificar(this);
+						novaPos.x += intersec.width;
+						vel = (sf::Vector2f(static_cast<float>(pIn->getImpacto()), static_cast<float>(-pIn->getImpacto() / 2)));
+					}
+				}
+				else //colisao vertical
+				{
+					if (inimigoBounds.top < jogadorBounds.top) //inimigo em cima
+					{
+						pIn->danificar(this);
+						novaPos.y -= intersec.height;
+						pIn->setVel(sf::Vector2f(pIn->getVel().x, -pIn->getVel().y));
+					}
+					else //inimigo embaixo
+					{
+						danificarInim(pIn);
+						pos.y -= intersec.height; //só aqui o jogador que ajusta sprite
+						vel.y = -vel.y;
+					}
+				}
 
-		void Jogador::controlar()
+				pIn->setPos(novaPos);
+			}
+		}
+
+
+        void Jogador::operator++()
 		{
-			//int keyTimeMax = 1; // delay para mover novamente //faço como atributo(???)
-			//posição mouse //para debugar
-			/* sf::Vector2i posMouse = sf::Mouse::getPosition(*pGG->getWindow());
-			setPos(static_cast<float>(posMouse.x), static_cast<float>(posMouse.y));*/
+			pontos++;
+		}
 
-
-			//implementar para ao invés de aumentar a velocidade, o controle modificar a aceleração ate a velocidade limite.
-			//velocidade = velocidade + aceleração;
-			//posiçao = posiçao + velocidade; // talvez esse seja melhor para padronizar. Teria que fazer um setPosicao.
-			//.move(velocidade); um dos dois.
-			//implementar o mesmo para outras entidades.
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && vel.x > (-velMovMax) )
+		void Jogador::controlar(char tecla)
+		{
+            if (tecla == 'A' && vel.x > (-velMovMax))
 			{
 				vel.x -= velMovMax;
 				direcao = false;
 			}
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && vel.x < velMovMax )
+			if (tecla == 'D' && vel.x < velMovMax )
 			{
+				
 				vel.x += velMovMax;
 				direcao = true;
 			}
 			
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && chao )
+			if (tecla == 'W' && chao)
 			{
-				vel.y -= 10 * velMovMax;
+				vel.y -= 5 * velMovMax;
 			}
-			/*
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && vel.y < velMovMax)
-				//&& timer >= keyTimeMax)
-			{
-				vel.y += velMovMax / 10; //na pratica so a gravidade acelera
-				//timer = 0;
-			}
-			*/
-			//timer++;
-			if (!sf::Keyboard::isKeyPressed(sf::Keyboard::A) && !sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+			
+			if (tecla == 'Z')
 			{
 				vel.x = 0;
 			}
 
-			//if(jog2) //para jogador 2
-		}
-			//é possivel implementar tambem sem haver uma velocidade maxima mas com a desaceleraçao naturalmente limitando
 
+		}
+			
+		Entidades::Teia* Jogador::atirarTeia() { 
+			if (tempoCooldown <= 0.0f && getAtirar()) {
+				 cout << "Criando nova teia..." << endl;
+				 sf::Vector2f posicaoTeia;
+				if (direcao) 
+				{ 
+					
+					posicaoTeia = sf::Vector2f(pos.x + 25.0f, 384.0f + 8.0f);//ajeitar aqui...
+				} 
+				else 
+				{ 
+					posicaoTeia = sf::Vector2f(pos.x - 5.0f, 384.0f + 8.0f);//ajeitar aqui
+				}
+				Entidades::Teia* novaTeia = new Entidades::Teia(posicaoTeia); 
+				float velHorizontal = 5.0f; 
+				if (!direcao) { 
+					velHorizontal = -velHorizontal;
+				}
+					
+				novaTeia->setVel(sf::Vector2f(velHorizontal, 0.0f)); 
+				tempoCooldown = cooldownTiro;
+				setAtirar(false);
+				return novaTeia; 
+			}
+			return NULL;
+	   }
+       
+	   void Jogador:: setAtirar(bool t)
+	   {
+		 tiro = t;
+	   }
+
+	   bool Jogador::getAtirar()
+	   {
+		 return tiro;
+	   }
 
 		void Jogador::mover()
 		{
-			controlar();   
-            acelerar();   
+		
+            acelerar(); 
+			
             atualizaVel(); 
+			
             atualizaPos();
+		
+			 
         }
 
-        void Jogador::setPontos(int p)
-        {
-            pontos = p;
-        }
 
-      
+
+       
 		void Jogador::executar()
 		{
+			
+			
 			mover();
+
+					
+		
 		}
 	}
 }
