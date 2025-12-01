196f4be309dd1debc3beb502128a9a703863e085
isabellaeurich <eurichisabella@gmail.com>
2025-11-30 21:58:16 -0300

    mudei

--- src/Personagem.cpp
+++ src/Personagem.cpp
@@ -1,9 +1,46 @@
 #include "Personagem.hpp"
-using namespace Entidades;
-using namespace Personagens;
+#include "ListaEntidades.hpp"
+#include "Projetil.hpp"
 
-Personagem::Personagem():Entidade(),num_vidas(-1){}
-Personagem::~Personagem(){}
+namespace Entidades
+{
+	namespace Personagens
+	{
 
-void Personagem::salvarDataBuffer(){}//...
-//...
+		Personagem::Personagem() :Entidade(sf::Vector2f(0.0, 0.0)), num_vidas(0), impacto(0), timer(0),  disparar(false),lista_projeteis(NULL){}
+
+        Personagem::Personagem(sf::Vector2f pos, Listas::ListaEntidades* p) :Entidade(pos), num_vidas(0), impacto(0),timer(0),  disparar(false), lista_projeteis(p) {}
+
+		Personagem::~Personagem() {}
+
+		void Personagem::setVidas(int vidas)
+		{
+			num_vidas = vidas;
+		}
+
+		void Personagem::perdeVida(int i)
+		{
+			num_vidas -= i;
+		}
+
+		int Personagem::getVidas()const
+		{
+			return num_vidas;
+		}
+
+		int Personagem::getImpacto()const
+		{
+			return impacto;
+		}
+
+		void Personagem::setDisparo(bool d)
+		{
+			disparar = d;
+		}
+
+		bool Personagem::getDisparo()const
+		{
+			return disparar;
+		}
+	}
+}
