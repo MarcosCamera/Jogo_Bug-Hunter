b5dc45e0fb95af1524c5d3f0f051d29af994c6c4
isabellaeurich <eurichisabella@gmail.com>
2025-11-30 21:50:28 -0300

    mudei

--- src/Jogador.hpp
+++ src/Jogador.hpp
@@ -1,5 +1,7 @@
 #pragma once
+#include "ListaEntidades.hpp"
 #include "Personagem.hpp"
+#include "Teia.hpp"
 #include "Inimigo.hpp"
 #include <iostream>  //precisa desses includes que foram adicionados nas primitivas???
 #include <SFML/Graphics.hpp>
@@ -15,9 +17,13 @@ namespace Entidades
         {
         protected:
             int pontos; 
+            float cooldownTiro;
+            float tempoCooldown;
+            bool tiro;
 
         public:
-            Jogador(sf::Vector2f posicao);
+            Jogador();
+            Jogador(sf::Vector2f pos = sf::Vector2f(0.0f, 0.0f), Listas::ListaEntidades* p = NULL);
             ~Jogador();
 
             void danificarInim(Inimigo* pIn);
@@ -27,9 +33,10 @@ namespace Entidades
             void controlar(char tecla);
             void mover();
             void executar();
-        
-            //json toJson();//salvamento
 
+            Entidades::Teia* atirarTeia();
+            void setAtirar(bool t);
+            bool getAtirar();
         };
     }
 }
