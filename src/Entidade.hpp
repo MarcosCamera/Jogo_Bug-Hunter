6e030d214300d45bebe82709fbb93de7e0c3d218
isabellaeurich <eurichisabella@gmail.com>
2025-11-30 21:29:19 -0300

    Update Entidade.hpp

--- src/Entidade.hpp
+++ src/Entidade.hpp
@@ -1,26 +1,30 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "Ente.hpp"
-
+#include <fstream> 
+#include "nlohmann/json.hpp" 
 
 
 namespace Entidades{
+
 class Entidade : public Ente{
    protected:
-  sf::Vector2f pos;
-  sf::Vector2f vel;
-  sf::Vector2f acel;
-  const float gravidade;
-  float normal;
-  const float arrasto;
-  float velMovMax; 
-  bool direcao; 
-  bool chao; 
-      
+     sf::Vector2f pos;
+     sf::Vector2f vel;
+     sf::Vector2f acel;
+     const float gravidade;
+     float normal;
+     const float arrasto;
+     float velMovMax; 
+     bool direcao; 
+     bool chao; 
+   
+     bool remover;//marca para quando precisa remover 
+         
 
     public:
 
-      Entidade(sf::Vector2f posicao);//sobrecarga construtora
+      Entidade(sf::Vector2f posicao);
       virtual ~Entidade();
 
       virtual void executar() = 0;
@@ -42,7 +46,11 @@ class Entidade : public Ente{
       bool getDir() const;
       void setDir(bool d);
 
+      bool getRemover() const { return remover; }
+      void setRemover(bool r) { remover = r; }
+
     
       
   };
 }
+
