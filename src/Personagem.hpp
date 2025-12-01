6f7ccca7ae3a862e1a4b0185adf19a1effeb9918
isabellaeurich <eurichisabella@gmail.com>
2025-11-30 21:57:48 -0300

    mudei

--- src/Personagem.hpp
+++ src/Personagem.hpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include "Entidade.hpp"
 
+
+
+namespace Listas { class ListaEntidades; }
 namespace Entidades 
 {
     namespace Personagens
@@ -14,25 +17,29 @@ namespace Entidades
         protected:
             int num_vidas;
             int impacto;
-            int timer;
+            int timer; 
             bool disparar;
+
+            Listas::ListaEntidades* lista_projeteis;
+            
 
         public:
             Personagem();
-            Personagem(sf::Vector2f pos);
+            Personagem(sf::Vector2f pos,Listas::ListaEntidades* p = NULL);
             virtual ~Personagem();
-
-            //quicar ~bounce
-            //empurrao ~knockback
-
-            void perdeVida(int i);
-            int getVida()const;
+            void setVidas(int vidas);
+            int getVidas()const;
+            void perdeVida(int i); 
+            
             int getImpacto()const;
             void setDisparo(bool d);
             bool getDisparo()const;
 
+
             virtual void mover() = 0;
             virtual void executar() = 0;
+            
+
         };
     }
 }
