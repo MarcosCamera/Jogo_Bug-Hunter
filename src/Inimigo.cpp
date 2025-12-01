cab6fe42dc3827d1f8e42eb3392d26f70353050d
isabellaeurich <eurichisabella@gmail.com>
2025-11-30 21:49:16 -0300

    Update Inimigo.cpp

--- src/Inimigo.cpp
+++ src/Inimigo.cpp
@@ -1,8 +1,35 @@
 #include "Inimigo.hpp"
-#include <SFML/Graphics.hpp>
 
+namespace Entidades
+{
+	namespace Personagens
+	{
 
+		Inimigo::Inimigo() :Personagem(sf::Vector2f(0.0f, 0.0f), NULL),nivel_maldade(0){}
 
-Entidades::Personagens::Inimigo::Inimigo():nivel_maldade(0){}
-Entidades::Personagens::Inimigo:: ~Inimigo(){}
+		Inimigo::Inimigo(sf::Vector2f pos, Listas::ListaEntidades* p ) : Personagem(pos, p), nivel_maldade(0){}
+		Inimigo:: ~Inimigo() {}
+        
+		void Inimigo::setMaldade(int m)
+		{
+			nivel_maldade = m;
+		}
 
+		int Inimigo::getMaldade()
+		{
+			return nivel_maldade;
+		}
+		
+		void Inimigo::operator++()
+		{
+			nivel_maldade++;
+		}
+
+		void Inimigo::operator--()
+		{
+			nivel_maldade--;
+		}
+
+		
+	}
+}
